1251cf93d376c6acea8952b74bb3ff378013ee49
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-24 14:31:31 +0000

    Ability to sign up and auto-login users

--- app/server.rb
+++ app/server.rb
@@ -3,8 +3,12 @@ require 'data_mapper'
 env = ENV["RACK_ENV"] || "development"
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 require './lib/link'
+require './lib/user'
 DataMapper.finalize
 DataMapper.auto_upgrade!
+
+enable :sessions
+set :session_secret, 'super secret'
 
 
 get '/' do
@@ -17,11 +21,27 @@ post '/links' do
   title = params["title"]
   tags = params["tags"].split(" ").map { |tag| Tag.first_or_create(:text => tag) }
   Link.create(:url => url, :title => title, :tags => tags)
-  redirect to ('/')
+  redirect to('/')
 end
 
 get '/tags/:text' do
   tag = Tag.first(:text => params[:text])
   @links = tag ? tag.links : []
   erb :index
+end
+
+get '/users/new' do
+  erb :"users/new"
+end
+
+post '/users' do
+  user = User.create(:email => params[:email], :password => params[:password])
+  session[:user_id] = user.id
+  redirect to('/')
+end
+
+helpers do
+  def current_user
+    @current_user ||=User.get(session[:user_id]) if session[:user_id]
+  end
 end
