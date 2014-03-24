5bf339ee0e3aff93b58161b851569e66137972e6
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-24 15:39:12 +0000

    Fixed stupid errors that broke my poor rspec tests

--- lib/user.rb
+++ lib/user.rb
@@ -5,11 +5,11 @@ class User
   attr_reader :password
   attr_accessor :password_confirmation
 
-  validates_confirmation_of :password
+  validates_confirmation_of :password, :message => "Sorry, your passwords don't match"
 
   property :id, Serial
-  property :email, String
-  property :password_digest, Text, :unique => true, :message => "This email is already taken"
+  property :email, String, :unique => true, :message => "This email has already been taken"
+  property :password_digest, Text
 
   def password=(password)
     @password = password
