e0e77bff40f806790c6f16a921193842f0601606
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-25 17:32:10 +0000

    Fixed broken test and extracted footer to a partial

--- spec/features/listing_all_links_spec.rb
+++ spec/features/listing_all_links_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'User browses the list of links' do
   before(:each){
-    Link.create(:url => "http://www.makersacademy.com", :title => "Makers Academy", :tags => [Tag.first_or_create(:text => 'education')])
+    Link.create(:url => "http://www.lse.ac.uk", :title => "London School of Economics", :tags => [Tag.first_or_create(:text => 'education')])
     Link.create(:url => "http://www.google.com", :title => "Google", :tags => [Tag.first_or_create(:text => 'search')])
     Link.create(:url => "http://www.bing.com", :title => "Bing", :tags => [Tag.first_or_create(:text => 'search')])
     Link.create(:url => "http://www.code.org", :title => "Code.org", :tags => [Tag.first_or_create(:text => 'education')])
@@ -10,12 +10,12 @@ feature 'User browses the list of links' do
 
   scenario "When opening the home page" do
     visit '/'
-    expect(page).to have_content("Makers Academy")
+    expect(page).to have_content("London School of Economics")
   end
 
   scenario "filtered by a tag" do
     visit '/tags/search'
-    expect(page).not_to have_content("Makers Academy")
+    expect(page).not_to have_content("London School of Economics")
     expect(page).not_to have_content("Code.org")
     expect(page).to have_content("Google")
     expect(page).to have_content("Bing")
