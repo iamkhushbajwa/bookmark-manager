f972762e645260aa4487aa34272328478bf0716f
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-27 18:17:01 +0000

    Added property description to Link

--- lib/link.rb
+++ lib/link.rb
@@ -4,6 +4,7 @@ class Link
   property :id, Serial
   property :title, String, :required => true
   property :url, String, :required => true
+  property :description, Text
   property :user_id, String
   has n, :tags, :through => Resource
   has n, :users, :through => Resource
