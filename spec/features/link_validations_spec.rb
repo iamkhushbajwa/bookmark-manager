3b34240151279fce708314794ad5cbb9b1e35824
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-27 16:23:33 +0000

    Added extra link validation

---
+++ spec/features/link_validations_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require_relative 'helpers/link'
+
+include LinkHelpers
+
+feature "Link fails to satisfy validations" do
+  scenario "Link must have a title" do
+    lambda {add_link("http://www.makersacademy.com/", "")}.should change(Link, :count).by(0)
+  end
+end
