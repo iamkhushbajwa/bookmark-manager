90d0c113f8834a2e6d178313f38a7906e1d5a4af
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-27 16:53:25 +0000

    Link must have a url

--- lib/link.rb
+++ lib/link.rb
@@ -3,7 +3,7 @@ class Link
 
   property :id, Serial
   property :title, String, :required => true
-  property :url, String
+  property :url, String, :required => true
   property :user_id, String
   has n, :tags, :through => Resource
   has n, :users, :through => Resource
--- spec/features/link_validations_spec.rb
+++ spec/features/link_validations_spec.rb
@@ -7,4 +7,8 @@ feature "Link fails to satisfy validations" do
   scenario "Link must have a title" do
     lambda {add_link("http://www.makersacademy.com/", "")}.should change(Link, :count).by(0)
   end
+
+  scenario "Link must have a URL" do
+    lambda {add_link("", "Makers Academy")}.should change(Link, :count).by(0)
+  end
 end
