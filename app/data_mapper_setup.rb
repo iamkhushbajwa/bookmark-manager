d3a65febf51dd1f4c700ae2280f4231a4cdcd4dd
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-28 14:42:05 +0000

    Using environment variable

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,7 +1,7 @@
 env = ENV["RACK_ENV"] || "development"
 
 if env == "production"
-  DataMapper.setup(:default, "postgres://ptlkvhbohxzoig:Xvh-0E-O1ynUX4D6_OLUhdXCfp@ec2-54-217-208-135.eu-west-1.compute.amazonaws.com:5432/ddn1h1f6fhbue0")
+  DataMapper.setup(:default, ENV["DATABASE_URL"])
   DataMapper.finalize
   DataMapper.auto_upgrade!
 else
