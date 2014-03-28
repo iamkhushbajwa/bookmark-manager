a20e2c5e3ceac42c0bb8214f704afa20b191df65
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-28 14:38:55 +0000

    Updated datamapper settings

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,4 +1,11 @@
 env = ENV["RACK_ENV"] || "development"
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-DataMapper.finalize
-DataMapper.auto_upgrade!
+
+if env == "production"
+  DataMapper.setup(:default, "postgres://ptlkvhbohxzoig:Xvh-0E-O1ynUX4D6_OLUhdXCfp@ec2-54-217-208-135.eu-west-1.compute.amazonaws.com:5432/ddn1h1f6fhbue0")
+  DataMapper.finalize
+  DataMapper.auto_upgrade!
+else
+  DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+  DataMapper.finalize
+  DataMapper.auto_upgrade!
+end
