a557f4aee8b4b9c1f5554fb5b4c7f58d21c78e3d
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-04-02 12:23:52 +0100

    Moved message about adding/removing to a notice flash

--- app/public/javascript/application.js
+++ app/public/javascript/application.js
@@ -1,3 +1,11 @@
+function showLinkFavouritedNotice(link){
+  var favourited = !!$(link).data("favourited");
+  var name = $(link).find('.title').text();
+  var message = favourited ? name + " was added to favourites" : name + " was removed from favourites";
+  var $flash = $("<div></div>").addClass('flash notice').html(message);
+  $flash.appendTo('#flash-container');
+}
+
 function addFavouritesHandler(){
   $(".star.solid").click(function(event){
     var link = $(this).parent();
@@ -5,6 +13,7 @@ function addFavouritesHandler(){
     var newOpacity = favourited ? 0 : 1;
     $(link).data("favourited", !favourited);
     $(this).animate({opacity: newOpacity}, 1000);
+    showLinkFavouritedNotice(link);
   });
 }
 
