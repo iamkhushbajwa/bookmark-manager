633e447dcd7f1cae12576988f53c7a588cef5861
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-04-02 12:11:12 +0100

    Changed condition for setting opacity

--- app/public/javascript/application.js
+++ app/public/javascript/application.js
@@ -1,6 +1,9 @@
 function addFavouritesHandler(){
-  $(".star.solid").click(function(){
-    var newOpacity = 1 - parseInt($(this).css('opacity'));
+  $(".star.solid").click(function(event){
+    var link = $(this).parent();
+    var favourited = !!$(link).data("favourited");
+    var newOpacity = favourited ? 0 : 1;
+    $(link).data("favourited", !favourited);
     $(this).animate({opacity: newOpacity}, 1000);
   });
 }
